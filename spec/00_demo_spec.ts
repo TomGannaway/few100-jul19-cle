660ddf29077d34ef3209c51590207cd09b68d020
TomGannaway <tom.e.gannaway@gmail.com>
2019-07-15 20:05:49 +0000

    done

--- spec/00_demo_spec.ts
+++ spec/00_demo_spec.ts
@@ -2,6 +2,6 @@ describe('writing some tests', () => {
     it('is easy when you know how', () => {
         const a = 10, b = 15;
         const answer = a + b;
-        expect(answer).toBe(25);
+        expect(answer).toBe(25); // another
     });
 });
